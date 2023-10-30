d4ebb42aa8d09d959a61896181826735e466c7d3
Mayur Rathod <mdridrdrr@gmail.com>
2023-10-30 09:57:32 +0530

    useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
 const Home = () => {
-    const clickMe = (ev) =>{
-        console.log("Click me",ev)
-    } 
-    const clickAgain = (ag,ev)=>{
-        console.log(`Hello ${ag}`,ev)
+    const [name, setName] = useState("Mayur")
+    const [age, setAge] = useState(22)
+    // let name = "Mayur";
+    // let age = 22
+    const clickMe = (ev,name,age)=>{
+        setName(name)
+        setAge(age)
     }
     return ( 
         <div className="home">
+            <input type="text" className="name" placeholder="Enter name"/>
+            <input type="number" className="age" placeholder="Enter age"/>
             Home
-            <button onClick={clickMe}>Click</button>
-            <button onClick={(ev)=>{clickAgain("Mayur",ev)}}>Click</button>
+            <p>My name is {name} and age is {age}</p>
+            <button onClick={(ev)=>{
+                let ageElement = document.querySelector('.age')
+                let nameElement = document.querySelector('.name')
+                let age = ageElement.value.trim()
+                let name = nameElement.value.trim()
+                clickMe(ev,name,age)
+                }}>Click</button>
         </div>
      );
 }
